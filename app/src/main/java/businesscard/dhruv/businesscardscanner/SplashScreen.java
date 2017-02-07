990b070daaeaee3c2869e7ca99ab34118cbf9040
Dhruv Rathi <dhruvrathi15@gmail.com>
2017-02-07 16:13:51 +0530

    Splash Screen Added

---
+++ app/src/main/java/businesscard/dhruv/businesscardscanner/SplashScreen.java
@@ -0,0 +1,32 @@
+package businesscard.dhruv.businesscardscanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import me.wangyuwei.particleview.ParticleView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        ParticleView mParticleView = (ParticleView) findViewById(R.id.splash);
+        mParticleView.startAnim();
+
+        mParticleView.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
+            @Override
+            public void onAnimationEnd() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
